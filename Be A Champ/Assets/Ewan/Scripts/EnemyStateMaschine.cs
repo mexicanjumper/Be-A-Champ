6438dfb9623e1360029e9ffea0a5c06843e76f37
mexicanjumper <110393431+mexicanjumper@users.noreply.github.com>
2026-08-26 17:30:03 +0200

    progress bar and state maschine
    added the progress bar too fill and the State Maschine

--- Be A Champ/Assets/Ewan/Scripts/EnemyStateMaschine.cs
+++ Be A Champ/Assets/Ewan/Scripts/EnemyStateMaschine.cs
@@ -1,19 +1,69 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyStateMaschine : MonoBehaviour
 {
     public BaseEnemy enemy;
 
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD,
+
+    }
+
+
+    public TurnState currentState;
+
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (currentState)
+        {
+            case (TurnState.PROCESSING):
+                UpgradeProgressBar();
+                break;
+            case (TurnState.ADDTOLIST):
+
+                break;
+            case (TurnState.WAITING):
+
+                break;
+            case (TurnState.SELECTING):
+
+                break;
+            case (TurnState.ACTION):
+
+                break;
+            case (TurnState.DEAD):
+
+                break;
+
+        }
+    }
+
+    void UpgradeProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
         
+       
+        if (cur_cooldown >= max_cooldown)
+        {
+            currentState = TurnState.ADDTOLIST;
+        }
     }
 }
--- Be A Champ/Assets/Ewan/Scripts/HeroStateMaschine.cs
+++ Be A Champ/Assets/Ewan/Scripts/HeroStateMaschine.cs
@@ -1,19 +1,71 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HeroStateMaschine : MonoBehaviour
 {
     public BaseHero hero;
+    
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum TurnState
     {
-        
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD,
+
     }
 
-    // Update is called once per frame
+
+    public TurnState currentState;
+
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+    public Image ProgressBar;
+    
+    void Start()
+    {
+        currentState = TurnState.PROCESSING;
+    }
+
+   
     void Update()
     {
-        
+        //Debug.Log(currentState);
+        switch (currentState)
+        {
+         case (TurnState.PROCESSING):
+                UpgradeProgressBar();
+         break; 
+         case (TurnState.ADDTOLIST):
+
+         break;
+         case(TurnState.WAITING): 
+                
+         break; 
+         case(TurnState.SELECTING):
+                
+         break;
+         case (TurnState.ACTION):
+               
+         break;
+         case (TurnState.DEAD):
+               
+         break;
+            
+        }
+    }
+
+    void UpgradeProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+        float calc_cooldown = cur_cooldown / max_cooldown;
+        ProgressBar.transform.localScale = new Vector3(Mathf.Clamp(calc_cooldown, 0,1), ProgressBar.transform.localScale.y, ProgressBar.transform.localScale.z);
+        if (cur_cooldown >= max_cooldown)
+        {
+            currentState = TurnState.ADDTOLIST;
+        }
     }
 }
