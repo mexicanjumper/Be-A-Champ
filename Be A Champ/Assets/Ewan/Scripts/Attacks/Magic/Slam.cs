f8f02600940db05ea271427e312ea78e30203547
mexicanjumper <110393431+mexicanjumper@users.noreply.github.com>
2026-09-08 14:26:33 +0200

    added abilityies

---
+++ Be A Champ/Assets/Ewan/Scripts/Attacks/Magic/Slam.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Slam: BaseAttacks
+{
+    public Slam()
+    {
+        attackName = " Fat Slam";
+        attackDescription = "Slams the enemy with great force.";
+        attackDamage = 45f;
+        attackCost = 0f;
+
+    }
+}
+
