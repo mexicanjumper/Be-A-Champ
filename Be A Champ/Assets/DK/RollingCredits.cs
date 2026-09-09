1dcaeffbaef403c34c977d60c61f49b86781c9f6
LikanoMakote <lmakote8@gmail.com>
2026-09-09 16:55:15 +0200

    Updated Credits scene
    Finished Updated credits

---
+++ Be A Champ/Assets/DK/RollingCredits.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RollingCredits : MonoBehaviour
+{
+
+    public float scrollSpeed = 10f;
+
+    private RectTransform rectTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed + Time.deltaTime);
+    }
+}
